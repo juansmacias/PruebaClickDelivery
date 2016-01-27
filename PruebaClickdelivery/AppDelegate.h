6f4e3a8841ada4c4a1d99959a54149da3fca29a4
Juan Sebastian <juansmacias@gmail.com>
2016-01-27 11:55:27 -0500

    Initial Commit

---
+++ PruebaClickdelivery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaClickdelivery
+//
+//  Created by Juan on 27/01/16.
+//  Copyright © 2016 Juan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
